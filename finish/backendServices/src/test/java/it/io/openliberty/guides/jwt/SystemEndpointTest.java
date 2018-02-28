069766f8d110a62f427f3fb07444f3abeeb73e98
JunyiSun <junyi.sun@mail.utoronto.ca>
2018-02-28 14:15:32 -0500

    Fixes on issue #4, 7  (#11)
    * fixed some issues in #7 and #9
    
    * more fix
    
    * update on README
    
    * remove extra lines
    
    * LATEST TO RELEASE
    
    * seperate tests
    
    * more edits

--- finish/backendServices/src/test/java/it/io/openliberty/guides/jwt/SystemEndpointTest.java
+++ finish/backendServices/src/test/java/it/io/openliberty/guides/jwt/SystemEndpointTest.java
@@ -23,9 +23,8 @@ import org.junit.Test;
 import it.io.openliberty.guides.jwt.util.TestUtils;
 import test.JwtVerifier;
 
-public class EndpointWithJwtTest {
+public class SystemEndpointTest {
 
-    private final String INVENTORY_HOSTS = "/inventory/systems";
     private final String SYSTEM_PROPERTIES = "/system/properties";
     private final String TESTNAME = "TESTUSER";
 
@@ -42,8 +41,6 @@ public class EndpointWithJwtTest {
     @Test
     public void testSuite() {
         this.testGetPropertiesWithJwt();
-        this.testEmptyInventoryWithJwt();
-        this.testHostRegistrationWithJwt();
     }
 
     public void testGetPropertiesWithJwt() {
@@ -65,42 +62,5 @@ public class EndpointWithJwtTest {
         System.out.println(responseJson.getString("os.name"));
         System.out.println(propUrl);
     }
-
-    public void testEmptyInventoryWithJwt() {
-        String invUrl = baseUrl + INVENTORY_HOSTS;
-        Response invResponse = TestUtils.processRequest(invUrl, "GET", null, authHeader);
-
-        assertEquals("HTTP response code should have been " + Status.OK.getStatusCode() + ".",
-                     Status.OK.getStatusCode(),
-                     invResponse.getStatus());
-
-        JsonObject responseJson = TestUtils.toJsonObj(invResponse.readEntity(String.class));
-
-        assertEquals("The inventory should be empty on application start",
-                     0,
-                     responseJson.getInt("total"));
-
-        System.out.println(responseJson.getInt("total"));
-        System.out.println(invUrl);
-    }
-
-    public void testHostRegistrationWithJwt() {
-        String invUrl = baseUrl + INVENTORY_HOSTS + "/localhost";
-        Response invResponse = TestUtils.processRequest(invUrl, "GET", null, authHeader);
-
-        assertEquals("HTTP response code should have been " + Status.OK.getStatusCode() + ".",
-                     Status.OK.getStatusCode(),
-                     invResponse.getStatus());
-
-        JsonObject responseJson = TestUtils.toJsonObj(invResponse.readEntity(String.class));
-
-        assertEquals("The inventory should get the os.name of localhost",
-                     System.getProperty("os.name"),
-                     responseJson.getString("os.name"));
-
-        System.out.println(responseJson.getString("os.name"));
-        System.out.println(invUrl);
-    }
-
 }
 // end::test[]

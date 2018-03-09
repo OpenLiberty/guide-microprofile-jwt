ea03ecee635e52bb56c7dcde68cae8907e5b6620
JunyiSun <junyi.sun@mail.utoronto.ca>
2018-03-09 16:54:53 -0500

    Update based on last reviews (#28)

--- finish/backendServices/src/main/java/io/openliberty/guides/system/SystemResource.java
+++ finish/backendServices/src/main/java/io/openliberty/guides/system/SystemResource.java
@@ -14,14 +14,11 @@
 package io.openliberty.guides.system;
 
 import java.util.Properties;
-
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
-
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import javax.annotation.security.RolesAllowed;
 
 @RequestScoped

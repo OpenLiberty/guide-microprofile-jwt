af112aa29deef5ba5b80357ea4764a9b127ff789
Griffin Hadfield <griffhadfield@gmail.com>
2020-11-03 16:39:44 -0500

    reverted changes to travis test script

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -14,7 +14,7 @@ set -euxo pipefail
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -pl system -q clean package liberty:create liberty:install-feature liberty:deploy
+mvn -q clean package liberty:create liberty:install-feature liberty:deploy
 
 
 ## Run the tests
@@ -24,7 +24,6 @@ mvn -pl system -q clean package liberty:create liberty:install-feature liberty:d
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn -pl system liberty:start
-mvn -pl system failsafe:integration-test
-mvn -pl system liberty:stop
-mvn -pl system failsafe:verify
+mvn liberty:start
+mvn failsafe:integration-test liberty:stop
+mvn failsafe:verify 

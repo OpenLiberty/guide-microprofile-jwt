e1ff3d53d716799ab10f38e8081b40a020a930f8
Austin Seto <austin.seto@ibm.com>
2020-11-26 10:32:05 -0500

    Changes required for daily builds

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while getopts t:d:b:u: flag;
+do
+    case "${flag}" in
+        t) DATE="${OPTARG}";;
+        d) DRIVER="${OPTARG}";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}";;
+    esac
+done
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml frontendUI/pom.xml
+cat system/pom.xml frontendUI/pom.xml
+
+../scripts/testApp.sh

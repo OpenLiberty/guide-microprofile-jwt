ba343aa123a67cf4db609bee62c8a41eef4fd8d0
Austin Seto <austin.seto@ibm.com>
2020-11-26 12:09:06 -0500

    Change test script

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -8,8 +8,9 @@ set -euxo pipefail
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -q clean package liberty:create liberty:install-feature liberty:deploy
-
+mvn -q clean install liberty:start-server
+sleep 30
+mvn liberty:stop-server
 
 ## Run the tests
 # These commands are separated because if one of the commands fail, the test script will fail and exit.
@@ -18,6 +19,4 @@ mvn -q clean package liberty:create liberty:install-feature liberty:deploy
 #       failsafe:integration-test - Runs the integration tests of an application.
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
-mvn liberty:start
-mvn failsafe:integration-test liberty:stop
-mvn failsafe:verify
+mvn verify
